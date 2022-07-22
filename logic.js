35de3c6537a8a0a96ef458af52a922fa435e9c5f
Doubl3K <kevin-kinner@web.de>
2022-07-22 18:22:03 +0200

    Basic append logic

--- logic.js
+++ logic.js
@@ -1,3 +1,21 @@
-const div = document.createElement("div");
-div.innerText = "This is made with JS"
-document.getElementById("test").appendChild(div);
+const gameBoard = (() =>{
+    const gameArr = ["X","X","X","X","X","X","X","X","X"]
+
+    const placeTile = (() =>{
+        document.getElementById("playground1").addEventListener("click", clicked);
+        document.getElementById("playground2").addEventListener("click", clicked);
+        document.getElementById("playground3").addEventListener("click", clicked);
+        document.getElementById("playground4").addEventListener("click", clicked);
+        document.getElementById("playground5").addEventListener("click", clicked);
+        document.getElementById("playground6").addEventListener("click", clicked);
+        document.getElementById("playground7").addEventListener("click", clicked);
+        document.getElementById("playground8").addEventListener("click", clicked);
+        document.getElementById("playground9").addEventListener("click", clicked);
+    })();
+    function clicked (){
+        
+        document.getElementById("playground1").textContent = "X";
+    }
+})();
+
+
