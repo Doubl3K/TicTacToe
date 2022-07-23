94c98e35adaa62a366fe1a7bd4a7ba4d5296b65b
Doubl3K <kevin-kinner@web.de>
2022-07-23 10:17:32 +0200

    Changed from if to own click fucntion for event

--- logic.js
+++ logic.js
@@ -2,20 +2,23 @@ const gameBoard = (() =>{
     const gameArr = ["X","X","X","X","X","X","X","X","X"]
 
     const placeTile = (() =>{
-        document.getElementById("playground1").addEventListener("click", clicked);
-        document.getElementById("playground2").addEventListener("click", clicked);
-        document.getElementById("playground3").addEventListener("click", clicked);
-        document.getElementById("playground4").addEventListener("click", clicked);
-        document.getElementById("playground5").addEventListener("click", clicked);
-        document.getElementById("playground6").addEventListener("click", clicked);
-        document.getElementById("playground7").addEventListener("click", clicked);
-        document.getElementById("playground8").addEventListener("click", clicked);
-        document.getElementById("playground9").addEventListener("click", clicked);
+        document.getElementById("playground1").addEventListener("click", clicked1);
+        document.getElementById("playground2").addEventListener("click", clicked2);
+        document.getElementById("playground3").addEventListener("click", clicked3);
+        document.getElementById("playground4").addEventListener("click", clicked4);
+        document.getElementById("playground5").addEventListener("click", clicked5);
+        document.getElementById("playground6").addEventListener("click", clicked6);
+        document.getElementById("playground7").addEventListener("click", clicked7);
+        document.getElementById("playground8").addEventListener("click", clicked8);
+        document.getElementById("playground9").addEventListener("click", clicked9);
     })();
-    function clicked (){
-        
+    function clicked1 (){
         document.getElementById("playground1").textContent = "X";
+    }
+    function clicked2 (){
+        document.getElementById("playground2").textContent = "X";
     }
 })();
 
 
+// find a way to attach eventlistener to field without if 
