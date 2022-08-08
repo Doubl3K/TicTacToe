77f32b26d44c8e76d1f5ceb181d5f81a32c27e62
Doubl3K <kevin-kinner@web.de>
2022-08-08 20:35:55 +0200

    Checking for winner after every move

--- logic.js
+++ logic.js
@@ -15,7 +15,7 @@ const player2 = createPlayers("Jeff");
 
 const startGame = () =>{
     console.log("the game has been started");
-    const gameArr = ["test","O","O","O","O","O","X","O","O"];
+    const gameArr = ["field1", "field2", "field3", "field4", "field5", "field6", "field7", "field8", "field9"];
     
     const turnLogic = (() =>{
         let playerTurnCupp = 0;
@@ -36,50 +36,68 @@ const startGame = () =>{
             let playerturn = turn();
             document.getElementById("playground1").textContent = (playerturn);
             document.getElementById("playground1").removeEventListener("click", playground1);
+            gameArr[0] = playerturn;
+            winnerLogic();
         });
         document.getElementById("playground2").addEventListener("click", playground2 = () =>{
             let playerturn = turn();
             document.getElementById("playground2").textContent = (playerturn);
             document.getElementById("playground2").removeEventListener("click", playground2);
+            gameArr[1] = playerturn;
+            winnerLogic();
         });
         document.getElementById("playground3").addEventListener("click", playground3 = () =>{
             let playerturn = turn();
             document.getElementById("playground3").textContent = (playerturn);
             document.getElementById("playground3").removeEventListener("click", playground3);
+            gameArr[2] = playerturn;
+            winnerLogic();
         });
         document.getElementById("playground4").addEventListener("click", playground4 = () =>{
             let playerturn = turn();
             document.getElementById("playground4").textContent = (playerturn);
             document.getElementById("playground4").removeEventListener("click", playground4);
+            gameArr[3] = playerturn;
+            winnerLogic();
         });
         document.getElementById("playground5").addEventListener("click", playground5 = () =>{
             let playerturn = turn();
             document.getElementById("playground5").textContent = (playerturn);
             document.getElementById("playground5").removeEventListener("click", playground5);
+            gameArr[4] = playerturn;
+            winnerLogic();
         });
         document.getElementById("playground6").addEventListener("click", playground6 = () =>{
             let playerturn = turn();
             document.getElementById("playground6").textContent = (playerturn);
             document.getElementById("playground6").removeEventListener("click", playground6);
+            gameArr[5] = playerturn;
+            winnerLogic();
         });
         document.getElementById("playground7").addEventListener("click", playground7 = () =>{
             let playerturn = turn();
             document.getElementById("playground7").textContent = (playerturn);
             document.getElementById("playground7").removeEventListener("click", playground7);
+            gameArr[6] = playerturn;
+            winnerLogic();
         });
         document.getElementById("playground8").addEventListener("click", playground8 = () =>{
             let playerturn = turn();
             document.getElementById("playground8").textContent = (playerturn);
             document.getElementById("playground8").removeEventListener("click", playground8);
+            gameArr[7] = playerturn;
+            winnerLogic();
         });
         document.getElementById("playground9").addEventListener("click", playground9 = () =>{
             let playerturn = turn();
             document.getElementById("playground9").textContent = (playerturn);
             document.getElementById("playground9").removeEventListener("click", playground9);
+            gameArr[8] = playerturn;
+            winnerLogic();
         }); 
     })();
 
-    const winnerLogic = (() =>{
+    const winnerLogic = () =>{
         //Horziontal winner check
             if (gameArr[0]==gameArr[1] && gameArr[0]==gameArr[2]) {
                 console.log("Winner in row 1");
@@ -102,7 +120,8 @@ const startGame = () =>{
             } else if (gameArr[2]==gameArr[4] && gameArr[2]==gameArr[6]) {
                 console.log("cross win from top right");
             }
-        })();
+            console.log(gameArr);
+        }
     }
 document.getElementById("start-button").addEventListener("click", startGame);
 
